87481f4d0ffaf11b12d573a6e15b126d13fc5cc2
yuta-0514 <y.akutsu0514@gmail.com>
2023-05-15 20:17:28 +0900

    CelebA

--- configs/test_r50.py
+++ configs/test_r50.py
@@ -19,7 +19,7 @@ config.lr = 0.1
 config.verbose = 2000
 config.dali = False
 
-config.rec = "/mnt/umd_face"
+config.rec = "/mnt/CelebA"
 config.num_classes = 8277
 config.num_image = 367888
 config.num_epoch = 20
--- setup/imagefolder.py
+++ setup/imagefolder.py
@@ -4,7 +4,7 @@ import shutil
 
 images = []
 labels = []
-with open('/mnt/faces_umd/faces_umd_path.txt') as f:
+with open('/mnt/CelebA/identity_CelebA.txt') as f:
     for line in f:
         sample = line.strip('\n').split(' ')
         images.append(sample[0])
@@ -12,14 +12,14 @@ with open('/mnt/faces_umd/faces_umd_path.txt') as f:
 
 
 for i, label in enumerate(labels):
-    folder_dir = os.path.join('/mnt/test/' + str(label))
+    folder_dir = os.path.join('/mnt/CelebA/' + str(label))
     if not os.path.isdir(folder_dir):
         os.mkdir(folder_dir)
     # copy image + rename
-    image = os.path.join('/mnt/faces_umd/faces_umd/', images[i])
+    image = os.path.join('/mnt/data/CelebA/img_celeba/', images[i])
     shutil.copy(image, folder_dir)
 
 # import glob
-# cnt = glob.glob('/mnt/umd_face/*/*.jpg')
+# cnt = glob.glob('/mnt/CelebA/*/*.jpg')
 # import pdb
 # pdb.set_trace()
