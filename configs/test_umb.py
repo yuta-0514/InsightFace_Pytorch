0eb90491fc4a483a579fa1d5381a7e66d1451b1f
yuta-0514 <y.akutsu0514@gmail.com>
2023-09-24 22:48:11 +0900

    PCGrad

--- configs/test_umb.py
+++ configs/test_umb.py
@@ -15,8 +15,8 @@ config.fp16 = True
 config.momentum = 0.9
 config.weight_decay = 5e-4
 config.batch_size = 6
-config.lr = 0.1
-config.verbose = 20000
+config.lr = 0.01
+config.verbose = 2000
 config.dali = False
 
 config.rec = "/mnt/umd_face"
